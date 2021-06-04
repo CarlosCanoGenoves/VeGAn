7f7ee6aa1dd8e6603f2e18009612cba895b37d0c
CarlosCanoGenoves <42547476+CarlosCanoGenoves@users.noreply.github.com>
2021-06-04 12:15:20 +0200

    Experiment

---
+++ GoalModel/src/Experiment/compareedX.java
@@ -0,0 +1,21 @@
+package Experiment;
+
+import VEGAN.UsingEMFModel;
+import VISUAL.LoadFile;
+import goalModel.Actor;
+import goalModel.EConfidence;
+import goalModel.EImportance;
+import goalModel.Goal;
+import goalModel.GoalModel;
+import goalModel.IntentionalElement;
+import goalModel.SoftGoal;
+import goalModel.Task;
+
+public class compareedX {
+
+public static void main(String[] argv) throws Exception {
+		
+	GoalModel goalModel = UsingEMFModel.load("");
+	}
+	
+}
