65bddf594a13d75133a97d1b484077345a0cd539
CarlosCanoGenoves <42547476+CarlosCanoGenoves@users.noreply.github.com>
2021-08-03 12:36:24 +0200

    piStar NEED TEST

--- GoalModel/src/piStar/jsonTest.java
+++ GoalModel/src/piStar/jsonTest.java
@@ -15,7 +15,8 @@ import goalModel.*;
 
 //https://www.cin.ufpe.br/~jhcp/pistar/tool/
 
-public class jsonTest {
+public class jsonTest
+{
 
 	public static void main(String[] args) throws JsonParseException, IOException {
 		JsonFactory jsonFactory = new JsonFactory();
@@ -24,33 +25,6 @@ public class jsonTest {
 
 		JsonNode jsonNode = jp.readValueAsTree();
 
-		JsonNode actors = jsonNode.get("actors");
-
-		for(int i=0;i< actors.size();i++)
-		{
-			JsonNode actor = actors.get(i);
-
-			System.out.println("Actor: " + actor.get("text").asText());
-
-			JsonNode nodes = actor.get("nodes");
-			for(int j = 0; j < nodes.size();j++)
-			{
-				JsonNode node = nodes.get(j);
-				System.out.println("IE: " + node.get("text").asText() + " Type: " + node.get("type").asText());
-			}
-
-			System.out.println();
-			System.out.println();
-		}
-
-		JsonNode actor1 = getActor(jsonNode, "3589ee55-603d-41ee-8bf1-2b2a54498def");
-		//System.out.println(actor1);
-		
-		JsonNode ie1 = getjIE(jsonNode, "71c7aeb6-fb99-40a1-bcd1-5a29e5b45252");
-		//System.out.println(ie1);	
-		
-		getSourceLinksFromIE(jsonNode, "8d716a61-1ca4-44f4-934c-26166ea44d11");
-		
 		GoalModel goalModel = generateGoalModel(jsonNode);
 		UsingEMFModel.save(goalModel, "./piStar/KGoalModel.xmi");
 	}
@@ -187,92 +161,31 @@ public class jsonTest {
 				}
 			}
 				break;
-				
-			case "istar.DependencyLink":
-				break;
-				
 			default:
 				break;
 			}
 		}
 		
+		links = jsonNode.get("dependencies");
+		
+		for(int i=0;i< links.size();i++)
+		{
+			JsonNode link = links.get(i);
+
+			IntentionalElement ieS = getIE(goalModel, jsonNode, link.get("source").asText());
+			IntentionalElement ieT = getIE(goalModel, jsonNode, link.get("target").asText());
+			
+			if(ieS == null || ieT == null)
+				continue;
+			
+			Dependency dep = factory.createDependency();
+			dep.setSrc(ieS);
+			dep.getTrgs().add(ieT);
+		}
+		
 		return goalModel;
 	}
-	
-	/***
-	 * 
-	 * @param jsonNode	FULL JSON
-	 * @param actorId
-	 * @return
-	 */
-	public static JsonNode getActor(JsonNode jsonNode, String actorId) {
 
-		JsonNode actors = jsonNode.get("actors");
-		
-		for(int i=0;i< actors.size();i++)
-		{
-			JsonNode actor = actors.get(i);
-
-			if(actor.get("id").asText().equals(actorId))
-				return actor;
-		}
-
-		return null;
-	}
-	
-	/***
-	 * 
-	 * @param jsonNode FULL JSON
-	 * @param ieId
-	 * @return
-	 */
-	public static JsonNode getActorWhereIsIE(JsonNode jsonNode, String ieId)
-	{
-		JsonNode actors = jsonNode.get("actors");
-
-		for(int i=0;i< actors.size();i++)
-		{
-			JsonNode actor = actors.get(i);
-
-			JsonNode nodes = actor.get("nodes");
-			for(int j = 0; j < nodes.size();j++)
-			{
-				JsonNode node = nodes.get(j);
-
-				if(node.get("id").asText().equals(ieId))
-					return actor;
-			}
-		}
-
-		return null;
-	}
-
-	/***
-	 * 
-	 * @param jsonNode FULL JSON
-	 * @param ieId
-	 * @return
-	 */
-	public static JsonNode getjIE(JsonNode jsonNode, String ieId)
-	{
-		JsonNode actors = jsonNode.get("actors");
-
-		for(int i=0;i< actors.size();i++)
-		{
-			JsonNode actor = actors.get(i);
-
-			JsonNode nodes = actor.get("nodes");
-			for(int j = 0; j < nodes.size();j++)
-			{
-				JsonNode node = nodes.get(j);
-
-				if(node.get("id").asText().equals(ieId))
-					return node;
-			}
-		}
-
-		return null;
-	}
 	
 	public static IntentionalElement getIE(GoalModel goalModel, JsonNode jsonNode, String ieId)
 	{
@@ -298,52 +211,4 @@ public class jsonTest {
 		return null;
 	}
 	
-	/***
-	 * 
-	 * @param jsonnode FULL JSON
-	 * @param ieID
-	 * @return
-	 */
-	public static ArrayList<JsonNode> getSourceLinksFromIE(JsonNode jsonNode, String ieID)
-	{
-		ArrayList<JsonNode> sourceLinks = new ArrayList<JsonNode>();
-		
-		JsonNode links = jsonNode.get("links");
-		
-		for(int i=0;i< links.size();i++)
-		{
-			JsonNode link = links.get(i);
-
-			if(link.get("source").asText().equals(ieID))
-			{
-				sourceLinks.add(link);
-				System.out.println("Added: "+ link);
-			}
-		}
-		
-		return sourceLinks;
-	}
-	
-	/***
-	 * Este método es para obtener los links de un Dependum
-	 * @param jsonNode
-	 * @param ieID
-	 * @return
-	 */
-	public static JsonNode getSourceLinkFromIE(JsonNode jsonNode, String ieID)
-	{
-		JsonNode links = jsonNode.get("links");
-		
-		for(int i=0;i< links.size();i++)
-		{
-			JsonNode link = links.get(i);
-
-			if(link.get("source").asText().equals(ieID))
-			{
-				return link;
-			}
-		}
-		
-		return null;
-	}
 }
